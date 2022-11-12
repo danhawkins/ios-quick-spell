8a148b62d7e715e4773594956fa459299b325923
Danny Hawkins <danny@quiqup.com>
2022-11-12 09:45:23 +0400

    Initial Commit

---
+++ Quick Spell/Quick_SpellApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quick_SpellApp.swift
+//  Quick Spell
+//
+//  Created by Danny Hawkins on 12/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct Quick_SpellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
